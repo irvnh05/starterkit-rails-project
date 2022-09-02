f5712c1e07a6bf114d74b590bbfc43342b20213b
irvan.hilmi <irvan.hilmi@pgn-solution.co.id>
2022-09-02 10:03:50 +0000

    update label & filed lampiran data kontak

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,4 +4,5 @@ class Contact < ApplicationRecord
   belongs_to :user, optional: true
   has_many :status_report
   has_many :data_company , dependent: :destroy
+  has_many_attached :file_lampiran
 end
